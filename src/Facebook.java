e933c20599bc757ffeb5ed6c6e85c39c757b093f
Himedere <nguyenkhacthe94@gmail.com>
2018-05-22 16:24:57 +0700

    Facade pattern example

---
+++ src/Facebook.java
@@ -0,0 +1,13 @@
+public class Facebook implements SocialShare {
+    private String message;
+
+    @Override
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    @Override
+    public void share() {
+        System.out.println("Share to Facebook: " + this.message);
+    }
+}
